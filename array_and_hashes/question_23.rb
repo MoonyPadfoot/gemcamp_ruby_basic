575124f6b1145cbc5a6890a085be78b3d0662ebc
Ren Ant <eurekavixen@gmail.com>
2024-09-10 16:10:27 +0800

    question 23: author of second book

---
+++ array_and_hashes/question_23.rb
@@ -0,0 +1,16 @@
+books = {
+  book_1: {
+    title: 'Harry Potter',
+    author: 'JK Rowlings'
+  },
+  book_2: {
+    title: 'Lord of the Rings',
+    author: 'Tolkien'
+  },
+  book_3: {
+    title: 'Crime and Punishment',
+    author: 'Fyodr Dostyevski'
+  }
+}
+
+puts books[:book_2][:author]
