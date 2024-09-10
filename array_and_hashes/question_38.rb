69f1e341ba6553b41da8876a15f5bbfaf91ef32b
Ren Ant <eurekavixen@gmail.com>
2024-09-10 16:59:36 +0800

    question 38: birthday from array matrix

---
+++ array_and_hashes/question_38.rb
@@ -0,0 +1,8 @@
+nums_matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  ['-', 0]
+]
+
+puts "#{nums_matrix[0][0]}#{nums_matrix[2][2]}#{nums_matrix[2][2]}#{nums_matrix[2][1]}#{nums_matrix[3][0]}#{nums_matrix[0][0]}#{nums_matrix[0][2]}#{nums_matrix[3][0]}#{nums_matrix[0][0]}#{nums_matrix[0][0]}"
