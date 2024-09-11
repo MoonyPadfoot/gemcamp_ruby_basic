2b8b84e18cf2e32346f2a57784f4acd1b3e265ef
Ren Ant <eurekavixen@gmail.com>
2024-09-11 18:42:11 +0800

    question 1: day of week

---
+++ conditional_statements/case/question_1.rb
@@ -0,0 +1,20 @@
+puts "Enter day of week:"
+
+day = gets.to_i
+
+case day
+when 1
+  puts "mon"
+when 2
+  puts 'tue'
+when 3
+  puts 'wed'
+when 4
+  puts 'thu'
+when 5
+  puts 'fri'
+when 6
+  puts 'sat'
+when 7
+  puts 'sun'
+end
