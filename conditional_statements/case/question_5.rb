d44ea188bbdfaeddac89f70427b9688232957372
Ren Ant <eurekavixen@gmail.com>
2024-09-11 18:55:35 +0800

    question 5: month and season

---
+++ conditional_statements/case/question_5.rb
@@ -0,0 +1,17 @@
+puts "Enter month:"
+month = gets.chomp
+
+case month
+when "January"
+  puts "New Year"
+when "February"
+  puts "Valentines"
+when "March"
+  puts "Fire prevention"
+when "November"
+  puts "Halloween"
+when "December"
+  puts "Winter"
+else
+  puts "not applicable"
+end
