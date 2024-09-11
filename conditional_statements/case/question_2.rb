0daf7cdd5cc26fe8b716a14a776843a0dcb3f631
Ren Ant <eurekavixen@gmail.com>
2024-09-11 18:46:40 +0800

    question 2: grades

---
+++ conditional_statements/case/question_2.rb
@@ -0,0 +1,15 @@
+puts "Enter grade:"
+grade = gets.chomp
+
+case grade
+when 'A'
+  puts "Excellent"
+when 'B'
+  puts "Good"
+when 'C'
+  puts "Fair"
+when 'D'
+  puts "Bad"
+when 'F'
+  puts "Fail"
+end
