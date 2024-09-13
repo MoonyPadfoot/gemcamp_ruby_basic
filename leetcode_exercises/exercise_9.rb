59eb7c83d3567383a26f431c24c6a29e7ffbcf8f
Ren Ant <eurekavixen@gmail.com>
2024-09-13 13:40:18 +0800

    exercise 9: best time to buy and sell stock

---
+++ leetcode_exercises/exercise_9.rb
@@ -0,0 +1,17 @@
+def max_profit(prices)
+  left = 0
+  right = 1
+  max_profit = 0
+
+  while right < prices.length
+    if prices[left] < prices[right]
+      profit = prices[right] - prices[left]
+      max_profit = [profit, max_profit].max
+    else
+      left = right
+    end
+    right += 1
+  end
+
+  return max_profit
+end
