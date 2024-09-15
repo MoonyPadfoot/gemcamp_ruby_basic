1ef0e059d04ae8d6a3ead24d3f8c8e4c17a3e8bb
Ren Ant <eurekavixen@gmail.com>
2024-09-16 07:48:41 +0800

    exercise 13: ugly number

---
+++ leetcode_exercises/exercise_13.rb
@@ -0,0 +1,17 @@
+def is_ugly(n)
+  if n < 1
+    return false
+  end
+
+  while n % 2 == 0
+    n /= 2
+  end
+  while n % 3 == 0
+    n /= 3
+  end
+  while n % 5 == 0
+    n /= 5
+  end
+
+  return n == 1
+end
